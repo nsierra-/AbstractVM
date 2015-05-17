9c4c5286692e3bd9a4179bfa9ff52b2ad3cf8cd9
Noe Sierra-Velasquez <nsierra-@e1r10p16.42.fr>
2015-05-17 07:51:14 +0200

    tabs to spaces

--- inc/SyntaxChecker.hpp
+++ inc/SyntaxChecker.hpp
@@ -2,7 +2,7 @@
              .'         `.
             :             :   File       : SyntaxChecker.hpp
            :               :  Creation   : 2015-05-17 05:05:27
-           :      _/|      :  Last Edit  : 2015-05-17 07:34:24
+           :      _/|      :  Last Edit  : 2015-05-17 07:50:49
             :   =/_/      :   Author     : nsierra-   
              `._/ |     .'    Mail       : nsierra-@student.42.fr  
           (   /  ,|...-'
@@ -18,82 +18,82 @@
 # include <vector>
 # include <sstream>
 
-class	SyntaxChecker
+class   SyntaxChecker
 {
-	public:
-		typedef bool (SyntaxChecker::* SCVerifyFun)(const std::smatch &, unsigned int);
-		SyntaxChecker(const std::string filename = "", bool valid = false);
-		~SyntaxChecker(void);
-		SyntaxChecker(SyntaxChecker const & src);
-		SyntaxChecker &operator=(SyntaxChecker const & rhs);
+    public:
+        typedef bool (SyntaxChecker::* SCVerifyFun)(const std::smatch &, unsigned int);
+        SyntaxChecker(const std::string filename = "", bool valid = false);
+        ~SyntaxChecker(void);
+        SyntaxChecker(SyntaxChecker const & src);
+        SyntaxChecker &operator=(SyntaxChecker const & rhs);
 
-		bool						isValid(void) const;
-		std::vector<std::string>	&getTokens(void);
-		void						setFilename(const std::string &filename);
-		void						analyzeFile(void);
+        bool                        isValid(void) const;
+        std::vector<std::string>    &getTokens(void);
+        void                        setFilename(const std::string &filename);
+        void                        analyzeFile(void);
 
-		class	InvalidInstructionException : public std::exception
-		{
-			public:
-				InvalidInstructionException();
-				InvalidInstructionException(InvalidInstructionException const &src);
-				virtual ~InvalidInstructionException(void) throw();
+        class   InvalidInstructionException : public std::exception
+        {
+            public:
+                InvalidInstructionException();
+                InvalidInstructionException(InvalidInstructionException const &src);
+                virtual ~InvalidInstructionException(void) throw();
 
-				InvalidInstructionException &operator=(InvalidInstructionException const &rhs);
+                InvalidInstructionException &operator=(InvalidInstructionException const &rhs);
 
-				virtual const char *what() const throw();
+                virtual const char *what() const throw();
 
-				size_t					lineNumber;
-				std::string				line;
-		};
+                size_t                  lineNumber;
+                std::string             line;
+        };
 
-		class	InvalidValueException : public std::exception
-		{
-			public:
-				InvalidValueException();
-				InvalidValueException(InvalidValueException const &src);
-				virtual ~InvalidValueException(void) throw();
+        class   InvalidValueException : public std::exception
+        {
+            public:
+                InvalidValueException();
+                InvalidValueException(InvalidValueException const &src);
+                virtual ~InvalidValueException(void) throw();
 
-				InvalidValueException &operator=(InvalidValueException const &rhs);
+                InvalidValueException &operator=(InvalidValueException const &rhs);
 
-				virtual const char *what() const throw();
+                virtual const char *what() const throw();
 
-				size_t					lineNumber;
-				std::string				line;
-		};
+                size_t                  lineNumber;
+                std::string             line;
+        };
 
-		class	FileOpeningFailException : public std::exception
-		{
-			public:
-				FileOpeningFailException();
-				FileOpeningFailException(FileOpeningFailException const &src);
-				virtual ~FileOpeningFailException(void) throw();
+        class   FileOpeningFailException : public std::exception
+        {
+            public:
+                FileOpeningFailException();
+                FileOpeningFailException(FileOpeningFailException const &src);
+                virtual ~FileOpeningFailException(void) throw();
 
-				FileOpeningFailException &operator=(FileOpeningFailException const &rhs);
+                FileOpeningFailException &operator=(FileOpeningFailException const &rhs);
 
-				virtual const char *what() const throw();
+                virtual const char *what() const throw();
 
-				std::string				file;
-		};
+                std::string             file;
+        };
 
-	private:
-		std::string					_filename;
-		static const std::regex		_instructionPattern;
-		static const std::regex		_valuePattern;
+    private:
+        std::string                 _filename;
+        static const std::regex     _instructionPattern;
+        static const std::regex     _valuePattern;
 
-		bool										_valid;
-		std::vector<std::string>					_tokens;
-		std::vector<std::pair<unsigned int, SCVerifyFun>	>	_instructionsIndex;
+        bool                                        _valid;
+        std::vector<std::string>                    _tokens;
+        std::vector<std::pair<unsigned int, SCVerifyFun>    >   _instructionsIndex;
 
-		InvalidInstructionException	_invalidInstructionEx;
-		InvalidValueException		_invalidValueEx;
-		FileOpeningFailException	_fileOpeningFailEx;
+        InvalidInstructionException _invalidInstructionEx;
+        InvalidValueException       _invalidValueEx;
+        FileOpeningFailException    _fileOpeningFailEx;
 
-		bool						_instructionIsValid(const std::string &line, unsigned int lineNumber);
+        bool                        _instructionIsValid(const std::string &line, unsigned int lineNumber);
 
-		bool						_simpleValidation(const std::smatch &matches, unsigned int i);
-		bool						_pushValidation(const std::smatch &matches, unsigned int i);
-		bool						_assertValidation(const std::smatch &matche, unsigned int i);
+        bool                        _simpleValidation(const std::smatch &matches, unsigned int i);
+        bool                        _pushValidation(const std::smatch &matches, unsigned int i);
+        bool                        _assertValidation(const std::smatch &matche, unsigned int i);
 };
 
 #endif /* SYNTAXCHECKER_HPP */
--- src/AbstractVM.cpp
+++ src/AbstractVM.cpp
@@ -1,14 +1,14 @@
 /*             .-'''''-.
-			 .'         `.
-			:             :   File       : AbstractVM.cpp
-		   :               :  Creation   : 2015-05-17 03:41:30
-		   :      _/|      :  Last Edit  : 2015-05-17 03:56:05
-			:   =/_/      :   Author     : nsierra-   
-			 `._/ |     .'    Mail       : nsierra-@student.42.fr  
-		  (   /  ,|...-'
-		   \_/^\/||__
-		_/~  `""~`"` \_
-	 __/  -'/  `-._ `\_\__
+             .'         `.
+            :             :   File       : AbstractVM.cpp
+           :               :  Creation   : 2015-05-17 03:41:30
+           :      _/|      :  Last Edit  : 2015-05-17 07:50:54
+            :   =/_/      :   Author     : nsierra-   
+             `._/ |     .'    Mail       : nsierra-@student.42.fr  
+          (   /  ,|...-'
+           \_/^\/||__
+        _/~  `""~`"` \_
+     __/  -'/  `-._ `\_\__
    /jgs  /-'`  `\   \  \-*/
 
 #include "AbstractVM.hpp"
@@ -22,7 +22,7 @@ AbstractVM::AbstractVM()
 
 AbstractVM::AbstractVM(AbstractVM const & src)
 {
-	*this = src;
+    *this = src;
 }
 
 AbstractVM::~AbstractVM(void)
@@ -30,45 +30,45 @@ AbstractVM::~AbstractVM(void)
 
 }
 
-AbstractVM	&AbstractVM::operator=(AbstractVM const & rhs)
+AbstractVM  &AbstractVM::operator=(AbstractVM const & rhs)
 {
-	if (this != &rhs)
-	{
-		_syntaxChecker = rhs._syntaxChecker;
-	}
-	return *this;
+    if (this != &rhs)
+    {
+        _syntaxChecker = rhs._syntaxChecker;
+    }
+    return *this;
 }
 
-void		AbstractVM::setInputFile(const std::string &filename)
+void        AbstractVM::setInputFile(const std::string &filename)
 {
-	_syntaxChecker.setFilename(filename);
+    _syntaxChecker.setFilename(filename);
 }
 
-void		AbstractVM::checkFileIntegrity(void)
+void        AbstractVM::checkFileIntegrity(void)
 {
-	try {
-		_syntaxChecker.analyzeFile();
-	} catch (const SyntaxChecker::InvalidInstructionException &e) {
-		std::cout 
-			<< "\"" << e.line << "\""
-			<< " (line " << e.lineNumber << ") "
-			<< e.what() << std::endl
-		;
-		exit(EXIT_FAILURE);
-	} catch (const SyntaxChecker::InvalidValueException &e) {
-		std::cout 
-			<< "\"" << e.line << "\""
-			<< " (line " << e.lineNumber << ") "
-			<< e.what() << std::endl
-		;
-		exit(EXIT_FAILURE);
-	} catch (const SyntaxChecker::FileOpeningFailException &e) {
-		std::cout << "\"" << e.file << "\" " << e.what() << std::endl;
-		exit(EXIT_FAILURE);
-	}
+    try {
+        _syntaxChecker.analyzeFile();
+    } catch (const SyntaxChecker::InvalidInstructionException &e) {
+        std::cout 
+            << "\"" << e.line << "\""
+            << " (line " << e.lineNumber << ") "
+            << e.what() << std::endl
+        ;
+        exit(EXIT_FAILURE);
+    } catch (const SyntaxChecker::InvalidValueException &e) {
+        std::cout 
+            << "\"" << e.line << "\""
+            << " (line " << e.lineNumber << ") "
+            << e.what() << std::endl
+        ;
+        exit(EXIT_FAILURE);
+    } catch (const SyntaxChecker::FileOpeningFailException &e) {
+        std::cout << "\"" << e.file << "\" " << e.what() << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
-	std::cout << "LIL" << std::endl;
-	auto tmp = _syntaxChecker.getTokens();
-	for (auto tok : tmp)
-		std::cout << tok << std::endl;
+    std::cout << "LIL" << std::endl;
+    auto tmp = _syntaxChecker.getTokens();
+    for (auto tok : tmp)
+        std::cout << tok << std::endl;
 }
