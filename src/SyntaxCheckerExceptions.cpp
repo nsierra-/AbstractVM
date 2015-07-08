d847d70fb4bbe8218dd6a0749f2bd6a5b0fab88a
Noé Sierra-Velasquez <noesv42@gmail.com>
2015-07-08 08:40:22 +0200

    Compressed dat sheet

--- src/SyntaxCheckerExceptions.cpp
+++ src/SyntaxCheckerExceptions.cpp
@@ -2,7 +2,7 @@
 //             .'         `.
 //            :             :        File       : SyntaxCheckerExceptions.cpp
 //           :               :       Creation   : 2015-07-08 03:37:51
-//           :      _/|      :       Last Edit  : 2015-07-08 08:33:47
+//           :      _/|      :       Last Edit  : 2015-07-08 08:40:01
 //            :   =/_/      :        Author     : nsierra-
 //             `._/ |     .'         Mail       : nsierra-@student.42.fr
 //          (   /  ,|...-'
@@ -13,90 +13,33 @@
 
 #include "SyntaxChecker.hpp"
 
-SyntaxCheckerException::SyntaxCheckerException()
-{
-
-}
-
-SyntaxCheckerException::SyntaxCheckerException(SyntaxCheckerException const & src)
-{
-	*this = src;
-}
-
-SyntaxCheckerException::~SyntaxCheckerException(void) throw()
-{
-
-}
-
+SyntaxCheckerException::SyntaxCheckerException() {}
+SyntaxCheckerException::SyntaxCheckerException(SyntaxCheckerException const & src) { *this = src; }
+SyntaxCheckerException::~SyntaxCheckerException(void) throw() {}
 SyntaxCheckerException  &SyntaxCheckerException::operator=(SyntaxCheckerException const & rhs)
 {
 	lineNumber = rhs.lineNumber;
 	line = rhs.line;
 	return *this;
 }
-const char *	SyntaxCheckerException::what() const throw()
-{
- return "Syntax Checker Exception (You should not see this)";
-}
+const char *	SyntaxCheckerException::what() const throw() { return "Syntax Checker Exception (You should not see this)";}
 
 
-NoEndTokenException::NoEndTokenException()
-{
-
-}
-
-NoEndTokenException::NoEndTokenException(NoEndTokenException const & src)
-{
- *this = src;
-}
-
-NoEndTokenException::~NoEndTokenException(void) throw()
-{
-
-}
-
-const char *	NoEndTokenException::what() const throw()
-{
- return "End of program token (;;) not found.";
-}
+NoEndTokenException::NoEndTokenException() {}
+NoEndTokenException::NoEndTokenException(NoEndTokenException const & src) { *this = src; }
+NoEndTokenException::~NoEndTokenException(void) throw() {}
+const char *	NoEndTokenException::what() const throw() { return "End of program token (;;) not found.";}
 
 
-NeedlessEndTokenException::NeedlessEndTokenException()
-{
-
-}
-
-NeedlessEndTokenException::NeedlessEndTokenException(NeedlessEndTokenException const & src)
-{
- *this = src;
-}
-
-NeedlessEndTokenException::~NeedlessEndTokenException(void) throw()
-{
-
-}
-
-const char *	NeedlessEndTokenException::what() const throw()
-{
- return "Superfluous end of program token (;;) found. Use it only when using avm with stdin.";
-}
+NeedlessEndTokenException::NeedlessEndTokenException() {}
+NeedlessEndTokenException::NeedlessEndTokenException(NeedlessEndTokenException const & src) { *this = src; }
+NeedlessEndTokenException::~NeedlessEndTokenException(void) throw() {}
+const char *	NeedlessEndTokenException::what() const throw() { return "Superfluous end of program token (;;) found. Use it only when using avm with stdin.";}
 
 
-InvalidInstructionException::InvalidInstructionException()
-{
-
-}
-
-InvalidInstructionException::InvalidInstructionException(InvalidInstructionException const & src)
-{
- *this = src;
-}
-
-InvalidInstructionException::~InvalidInstructionException(void) throw()
-{
-
-}
-
+InvalidInstructionException::InvalidInstructionException() {}
+InvalidInstructionException::InvalidInstructionException(InvalidInstructionException const & src) { *this = src; }
+InvalidInstructionException::~InvalidInstructionException(void) throw() {}
 const char *	InvalidInstructionException::what() const throw()
 {
 	static std::stringstream   ss;
@@ -110,21 +53,9 @@ const char *	InvalidInstructionException::what() const throw()
 	return ss.str().c_str();
 }
 
-InvalidValueException::InvalidValueException()
-{
-
-}
-
-InvalidValueException::InvalidValueException(InvalidValueException const & src)
-{
- *this = src;
-}
-
-InvalidValueException::~InvalidValueException(void) throw()
-{
-
-}
-
+InvalidValueException::InvalidValueException() {}
+InvalidValueException::InvalidValueException(InvalidValueException const & src) { *this = src; }
+InvalidValueException::~InvalidValueException(void) throw() {}
 const char *	InvalidValueException::what() const throw()
 {
 	static std::stringstream   ss;
@@ -138,21 +69,9 @@ const char *	InvalidValueException::what() const throw()
 	return ss.str().c_str();
 }
 
-FileOpeningFailException::FileOpeningFailException()
-{
-
-}
-
-FileOpeningFailException::FileOpeningFailException(FileOpeningFailException const & src)
-{
- *this = src;
-}
-
-FileOpeningFailException::~FileOpeningFailException(void) throw()
-{
-
-}
-
+FileOpeningFailException::FileOpeningFailException() {}
+FileOpeningFailException::FileOpeningFailException(FileOpeningFailException const & src) { *this = src; }
+FileOpeningFailException::~FileOpeningFailException(void) throw() {}
 FileOpeningFailException &FileOpeningFailException::operator=(FileOpeningFailException const & rhs)
 {
 	file = rhs.file;
@@ -170,21 +89,9 @@ const char *	FileOpeningFailException::what() const throw()
 	return ss.str().c_str();
 }
 
-EmptyProgramException::EmptyProgramException()
-{
-
-}
-
-EmptyProgramException::EmptyProgramException(EmptyProgramException const & src)
-{
-	*this = src;
-}
-
-EmptyProgramException::~EmptyProgramException(void) throw()
-{
-
-}
-
+EmptyProgramException::EmptyProgramException() {}
+EmptyProgramException::EmptyProgramException(EmptyProgramException const & src) { *this = src; }
+EmptyProgramException::~EmptyProgramException(void) throw() {}
 const char *	EmptyProgramException::what() const throw()
 {
 	return "Program seems empty.";
